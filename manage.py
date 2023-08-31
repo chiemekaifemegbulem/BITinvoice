92732757322e9cedee45315c6d06d79ec18e5cd0
faykey <77450186+faykey@users.noreply.github.com>
2023-09-01 00:10:44 +0100

    Update on manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invoicing.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bitinvoice.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
