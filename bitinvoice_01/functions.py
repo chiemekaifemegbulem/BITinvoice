7e5b55e2cfca446ddd53c73dce991cd0b017c728
faykey <77450186+faykey@users.noreply.github.com>
2023-08-31 23:49:20 +0100

    Update on functions.py

--- bitinvoice_01/functions.py
+++ bitinvoice_01/functions.py
@@ -5,14 +5,14 @@ from django.conf import settings
 
 def emailInvoiceClient(to_email, from_client, filepath):
     from_email = settings.EMAIL_HOST_USER
-    subject = '[Skolo] Invoice Notification'
+    subject = '[BITincoice] Invoice Notification'
     body = """
     Good day,
 
     Please find attached invoice from {} for your immediate attention.
 
     regards,
-    Skolo Online Learning
+    Bitinvoice
     """.format(from_client)
 
     message = EmailMessage(subject, body, from_email, [to_email])
